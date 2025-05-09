6ff224478a348130a6fbccc4c6b43542baf3feb7
Holden Oullette <6202965+houllette@users.noreply.github.com>
2025-05-08 19:05:30 -0600

    Removed deprecated OTP versions and bumped minimum supported Sobelow version (#2)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Sobelow.Mixfile do
     [
       app: :sobelow,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
