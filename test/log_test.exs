c19905e3ea043af472e711358b0e181e805ad75b
Tyler Young <tyler@tylerayoung.com>
2026-01-06 12:59:46 -0600

    Fix test

--- test/log_test.exs
+++ test/log_test.exs
@@ -34,7 +34,11 @@ defmodule SobelowTest.LogTest do
       variable: "var"
     ]
 
-    Sobelow.log_finding(finding, %Sobelow.Finding{type: "Some check", confidence: :high})
+    Sobelow.log_finding(finding, %Sobelow.Finding{
+      type: "Some check",
+      confidence: :high,
+      vuln_line_no: 1
+    })
 
     assert Jason.decode!(FindingLog.json("1.0.0")) == output
   end
